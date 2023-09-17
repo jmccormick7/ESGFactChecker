fd195b675924bdc9a354436f4ff34e3307ed7abe
Marek Pinto <marek.pinto@gmail.com>
2023-09-17 09:57:26 -0400

    Updated to Vertex AI on Google Cloud

--- summarize_pdf.py
+++ summarize_pdf.py
@@ -1,6 +1,7 @@
 from pathlib import Path as p
 
 from langchain import OpenAI
+from langchain.llms import VertexAI
 from langchain import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -9,10 +10,16 @@ from langchain.document_loaders import PyPDFLoader
 import urllib
 
 
+PROJECT_ID = "foret-399300"
+LOCATION = "us-east1" #e.g. us-central1
+
+import vertexai
+vertexai.init(project=PROJECT_ID, location=LOCATION)
+
 data_folder = p.cwd() / "data"
 p(data_folder).mkdir(parents=True, exist_ok=True)
 
-pdf_url = "https://www.chevron.com/-/media/shared-media/documents/chevron-sustainability-report-2022.pdf"
+pdf_url = "https://www.tesla.com/ns_videos/2022-tesla-impact-report-highlights.pdf"
 pdf_file = str(p(data_folder, pdf_url.split("/")[-1]))
 
 urllib.request.urlretrieve(pdf_url, pdf_file)
@@ -23,8 +30,9 @@ print(pdf_loader)
 pages = pdf_loader.load_and_split()
 text = "\n".join([(page if isinstance(page, str) else page.page_content) for page in pages])
 
-OPENAI_API_KEY = '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'
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+#OPENAI_API_KEY = ''
+#llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
+llm = VertexAI(model_name="text-bison@001")
 
 text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], 
  chunk_size=3000, chunk_overlap=100)
@@ -32,17 +40,16 @@ text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"],
 docs = text_splitter.create_documents([text])
 
 map_prompt_template = """
-                      Write a two sentence summary of the following text delimited by triple backquotes that includes the main points and any important details. Please consider the following information if it is present in the passage:
-              Key risks in investing in the company, Environmental positives and negatives, Social positives and negatives, Governance positives and negatives, Overall sentiment, Adherance to government regulations, Change over time. Also, focus on sentences with these keywords: ESG, Sustainability, Environment, Diversity, Climate, Equality, Carbon, Conscious, Responsibility, CSR, Environment, Social, and Governance, Green, Renewable, Recycle, Discrimination, Racism, Sexism.
+                      Take 2-3 bullet points from the following text delimited by triple backquotes that includes the most important details and facts for investors and environmentalists to consider. Please consider the following information if it is present in the passage:
+              Key risks in investing in the company, Environmental positives and negatives, Social positives and negatives, Governance positives and negatives, Adherance to government regulations. Also, focus on extracting sentences with these keywords delimited by parentheses: (ESG, Sustainability, Environment, Diversity, Climate, Equality, Carbon, Conscious, Responsibility, CSR, Environment, Social, and Governance, Green, Renewable, Recycle, Discrimination, Racism, Sexism).
                       ```{text}```
                       """
 
 map_prompt = PromptTemplate(template=map_prompt_template, input_variables=["text"])
 
 combine_prompt_template = """
-                      Write a one to three page summary of the following text delimited by triple backquotes.
-                      Return your response in bullet points which covers the key points of the text. Please consider the following information if it is present in the passage:
-              Key risks in investing in the company, Environmental positives and negatives, Social positives and negatives, Governance positives and negatives, Overall sentiment, Adherance to government regulations, Change over time. Also, focus on sentences with these keywords: ESG, Sustainability, Environment, Diversity, Climate, Equality, Carbon, Conscious, Responsibility, CSR, Environment, Social, and Governance, Green, Renewable, Recycle, Discrimination, Racism, Sexism. 
+                      Write a 10-20 bullet point summary of the following text delimited by triple backquotes.the most important details and facts for investors and environmentalists to consider. Please consider the following information if it is present in the passage:
+              Key risks in investing in the company, Environmental positives and negatives, Social positives and negatives, Governance positives and negatives, Adherance to government regulations. Also, focus on extracting sentences with these keywords delimited by parentheses: (ESG, Sustainability, Environment, Diversity, Climate, Equality, Carbon, Conscious, Responsibility, CSR, Environment, Social, and Governance, Green, Renewable, Recycle, Discrimination, Racism, Sexism).
                       ```{text}```
                       BULLET POINT SUMMARY:
                       """
